f352b2ef07fb407728f5f390ae6abe9a7e0cd55b
Damien <72918476+DamienPace15@users.noreply.github.com>
2026-03-24 13:31:01 +1100

    Delete test-app directory

--- test-app/index.ts
+++
@@ -1,7 +0,0 @@
-import * as anvil from '@anvil/aws';
-
-/* const bucket = new anvil.aws.Bucket('spike-bucket', {
-  dataClassification: 'non-sensitive',
-});
- */
-new anvil.aws.Bucket('a', { dataClassification: 'sss' });
